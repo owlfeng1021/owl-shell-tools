d4aa52958cd39f7fb41dd67419da440c144eafae
owlfeng <1033571362@qq.com>
2019-12-06 12:08:42 +0800

    init

---
+++ 01_auto_compiler/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    int cock,hen,chick;
+    for(cock=1;cock<20;cock++){
+        for(hen=1;hen<33;hen++ )
+            {
+            chick=100-cock-hen;
+            if((cock*5)+(hen*3)+(chick/3)==100)
+            printf("cock:%d hen:%d aawssdddds1sasdasdassssasdasdaq真21dddsdfsdfadasdasdsdsssssssasdss是鸡123吗1 chick:%d\n",cock,hen,chick);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
